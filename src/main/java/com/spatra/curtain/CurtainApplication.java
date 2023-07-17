0b1f6c1be435997f731420c8af149e12778326ba
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-17 12:57:57 +0900

    init commit

---
+++ src/main/java/com/spatra/curtain/CurtainApplication.java
@@ -0,0 +1,13 @@
+package com.spatra.curtain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurtainApplication.class, args);
+    }
+
+}
