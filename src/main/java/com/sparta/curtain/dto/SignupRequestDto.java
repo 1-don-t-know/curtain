f6536aea1d4f6c472cef7f5232a00a1e8ecdff9e
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-17 17:27:36 +0900

    패키지 변경 및 이메일 필드 중복 삭제

--- src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
@@ -1,4 +1,4 @@
-package com.spatra.curtain.dto;
+package com.sparta.curtain.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
@@ -12,6 +12,13 @@ import lombok.*;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SignupRequestDto {
+
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message = "유효한 이메일 주소가 아닙니다.")
+    @Pattern(regexp = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}\\b",
+            message = "유효한 이메일 주소가 아닙니다.")
+    private String email;
+
     @NotBlank
     @Size(min = 4, max = 10, message = "4자 이상, 10자 이하")
     @Pattern(regexp = "^[a-z0-9]*$",message = "알파벳 소문자(a~z), 숫자(0~9)")
@@ -21,10 +28,6 @@ public class SignupRequestDto {
     @Size(min = 8, max = 15, message = "8자 이상, 15자 이하")
     @Pattern(regexp = "^[a-z0-9]*$",message = "알파벳 대소문자(a~z, A~Z), 숫자(0~9)")
     private String password;
-
-    @Email
-    private String email;
-
     private boolean admin = false;
     private String adminToken = "";
     private String authKey;
