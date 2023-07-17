1f0a450a14dab5c861d5538e44bfd4fed3b66fb7
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-17 17:44:53 +0900

    인증이 된 유저만 이름으로 조회하는 메서드 추가

---
+++ src/main/java/com/sparta/curtain/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sparta.curtain.security;
+
+import com.sparta.curtain.entity.User;
+import com.sparta.curtain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    // 주어진 사용자명(username)을 통해 데이터베이스에서 해당 사용자 정보를 조회
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
