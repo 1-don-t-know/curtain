593c3e148772c995ddebe1674512a63d341ce2bc
iseungchul <tnsrb950@gmail.com>
2023-07-18 05:37:40 +0900

    게시물 crud (기본) 카태고리 구성 아직x

---
+++ src/main/java/com/spatra/curtain/dto/PostResponseDto.java
@@ -0,0 +1,27 @@
+package com.spatra.curtain.dto;
+
+
+import com.spatra.curtain.entity.Post;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private String email;
+
+    public PostResponseDto(Post post) {
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
+        this.email = post.getUser().getEmail();
+}
+}
---
+++ src/main/java/com/spatra/curtain/service/PostService.java
@@ -0,0 +1,77 @@
+package com.spatra.curtain.service;
+
+
+import com.spatra.curtain.dto.PostListResponseDto;
+import com.spatra.curtain.dto.PostRequestDto;
+import com.spatra.curtain.dto.PostResponseDto;
+import com.spatra.curtain.entity.Post;
+import com.spatra.curtain.entity.User;
+import com.spatra.curtain.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    //게시글 생성
+    public PostResponseDto createPost(PostRequestDto requestDto, User user) {
+        Post post = new Post(requestDto);
+        post.setUser(user);
+
+        postRepository.save(post);
+
+        return new PostResponseDto(post);
+    }
+    //게시글 전체 조회(카테고리별 x)
+    public PostListResponseDto getPosts() {
+        List<PostResponseDto> postList = postRepository.findAll().stream()
+                .map(PostResponseDto::new)
+                .collect(Collectors.toList());
+
+        return new PostListResponseDto(postList);
+    }
+    //게시글 단건 조회(카테고리별 x)
+    public PostResponseDto getPostById(Long id) {
+        Post post = findPost(id);
+
+        return new PostResponseDto(post);
+    }
+
+    //게시글 삭제(관리자)
+    public void deletePost(Long id, User user) {
+        Post post = findPost(id);
+        //게시글 작성자와 요청자가 같은지 또는 관리자인지 체크 -> 아닐시 예외 발생
+        if (!user.getRole().equals(UserRoleEnum.ADMIN) && !post.getUser().equals(user)) {
+            throw new RejectedExecutionException();
+        }
+
+        postRepository.delete(post);
+    }
+    //게시글 수정(관리자)
+    @Transactional
+    public PostResponseDto updatePost(Long id, PostRequestDto requestDto, User user) {
+        Post post = findPost(id);
+        //게시글 작성자와 요청자가 같은지 또는 관리자인지 체크 -> 아닐시 예외 발생
+        if (!user.getRole().equals(UserRoleEnum.ADMIN) && !post.getUser().equals(user)) {
+            throw new RejectedExecutionException();
+        }
+
+        post.setTitle(requestDto.getTitle());
+        post.setContent(requestDto.getContent());
+
+        return new PostResponseDto(post);
+    }
+
+    public Post findPost(long id) {
+        return postRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 게시글이 존재하지 않습니다")
+        );
+    }
+}
