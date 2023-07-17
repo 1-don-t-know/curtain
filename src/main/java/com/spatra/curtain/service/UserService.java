9197c999c3bb629088cbef8fbf025959d25c149b
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-17 15:52:22 +0900

    회원가입 Service

--- src/main/java/com/spatra/curtain/service/UserService.java
+++ src/main/java/com/spatra/curtain/service/UserService.java
@@ -1,4 +1,59 @@
 package com.spatra.curtain.service;
 
+import com.spatra.curtain.dto.AuthRequestDto;
+import com.spatra.curtain.dto.SignupRequestDto;
+import com.spatra.curtain.entity.User;
+import com.spatra.curtain.entity.UserRoleEnum;
+import com.spatra.curtain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // ADMIN_TOKEN
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    // 회원가입
+    // - username, password를 Client에서 전달받기
+    // - username은  `최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)`로 구성되어야 한다.
+    // - password는  `최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자`로 구성되어야 한다.
+    // - DB에 중복된 username이 없다면 회원을 저장하고 Client 로 성공했다는 메시지, 상태코드 반환하기
+    // - 회원 권한 부여하기 (ADMIN, USER) - ADMIN 회원은 모든 게시글, 댓글 수정 / 삭제 가능
+    public void signup(SignupRequestDto signupRequestDto) {
+        String username = signupRequestDto.getUsername();
+        String password = signupRequestDto.getPassword();
+        String email = signupRequestDto.getEmail();
+
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 Email 입니다.");
+        }
+
+        //사용자 Role
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (signupRequestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(signupRequestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 달라 등록이 불가합니다.");
+            }
+        } role = UserRoleEnum.ADMIN;
+
+        String authKey = signupRequestDto.getAuthKey();
+        User user = new User(username, password, email, role, authKey);
+    }
+    // 로그인
+    // - username, password를 Client에서 전달받기
+    // - DB에서 username을 사용하여 저장된 회원의 유무를 확인하고 있다면 password 비교하기
+    // - 로그인 성공 시, 로그인에 성공한 유저의 정보와 JWT를 활용하여 토큰을 발급하고,
+    // - 발급한 토큰을 Header에 추가하고 성공했다는 메시지, 상태코드 와 함께 Client에 반환하기
+
+
+    //로그아웃
+
 }
