800fc3c00617a25e03b8cc86f78d89617bdc7d3d
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-18 11:39:39 +0900

    코드수정

--- src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
@@ -18,6 +18,8 @@ public class SignupRequestDto {
     @Pattern(regexp = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}\\b",
             message = "유효한 이메일 주소가 아닙니다.")
     private String email;
+    private boolean admin = false;
+    private String adminToken = "";
 
     @NotBlank
     @Size(min = 4, max = 10, message = "4자 이상, 10자 이하")
@@ -28,8 +30,8 @@ public class SignupRequestDto {
     @Size(min = 8, max = 15, message = "8자 이상, 15자 이하")
     @Pattern(regexp = "^[a-z0-9]*$",message = "알파벳 대소문자(a~z, A~Z), 숫자(0~9)")
     private String password;
-    private boolean admin = false;
-    private String adminToken = "";
+
+
     private String authKey;
     private boolean isConfirm =false;
 }
