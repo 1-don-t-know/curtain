cb5a6dae8bc41fc33a12ac90d0a794ef5b1c0553
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-18 11:47:42 +0900

    코드수정

--- src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/curtain/dto/SignupRequestDto.java
@@ -12,26 +12,19 @@ import lombok.*;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SignupRequestDto {
-
-    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
-    @Email(message = "유효한 이메일 주소가 아닙니다.")
-    @Pattern(regexp = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}\\b",
-            message = "유효한 이메일 주소가 아닙니다.")
-    private String email;
-    private boolean admin = false;
-    private String adminToken = "";
-
-    @NotBlank
-    @Size(min = 4, max = 10, message = "4자 이상, 10자 이하")
-    @Pattern(regexp = "^[a-z0-9]*$",message = "알파벳 소문자(a~z), 숫자(0~9)")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",
+            message = "최소 4자 이상, 10자 이하이며 알파벳 소문자, 숫자로 구성되어야 합니다.")
     private String username;
 
-    @NotBlank
-    @Size(min = 8, max = 15, message = "8자 이상, 15자 이하")
-    @Pattern(regexp = "^[a-z0-9]*$",message = "알파벳 대소문자(a~z, A~Z), 숫자(0~9)")
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+{}:\"<>?,.\\\\/]{8,15}$",
+            message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자, 숫자로 구성되어야 합니다.")
     private String password;
 
+    @Email
+    private String email;
 
+    private boolean admin = false;
+    private String adminToken = "";
     private String authKey;
     private boolean isConfirm =false;
 }
